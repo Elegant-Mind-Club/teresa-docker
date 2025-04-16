0d72c9493657404da3cc535f8282b8de3c3ccb4c
Andy Ngo <andytpngo@g.ucla.edu>
2025-04-16 15:13:35 -0700

    optimize MuJoCo viewer to run at better frame rates

--- host/sim.py
+++ host/sim.py
@@ -1,11 +1,9 @@
 # MuJoCo Simulation
-import socket
-import json
 import threading
 import os
 import mujoco
-from models.robot import RobotArm, RobotJoint
 import zmq
+import time
 
 
 class Listener:
@@ -16,15 +14,16 @@ class Listener:
         context = zmq.Context()
         try:
             self.s = context.socket(zmq.PULL)
+            self.s.setsockopt(zmq.CONFLATE, 1)
             self.s.bind(f"tcp://*:{self.port}")
-        except:
-            print("Unable to create socket")
+        except Exception as e:
+            print(f"Unable to create socket: {e}")
             exit(-1)
 
     def start(self):
         self.listen_thread = threading.Thread(target=self._listen_loop)
         self.listen_thread.start()
-        print("Setup complete")
+        print("Listening to the container...")
 
     def _listen_loop(self):
         while True:
@@ -40,18 +39,19 @@ class Listener:
 # TODO: add simulated cameras
 class SimVis:
     def __init__(self):
+        self.SIM_TARGET_FPS = 30  # FPS
+        self.visualization_refresh_rate = 1 / self.SIM_TARGET_FPS  # Hz
         self.model = mujoco.MjModel.from_xml_path(
             os.path.join(os.path.dirname(__file__), "barebones_urdf/mjmodel.xml")
         )
         self.data = mujoco.MjData(self.model)
-        self.robot_arm = RobotArm(self.data, self.model)
-        # Run thread for listening
-        self.listener = Listener(self.set_absolute_position)
-        self.listener.start()
         # Run thread for visualization
         self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
         self.viewer_thread = threading.Thread(target=self.run_viewer)
         self.viewer_thread.start()
+        # Run thread for listening
+        self.listener = Listener(self.set_absolute_position)
+        self.listener.start()
 
     def set_joint(self, joint_name: str, joint_type: str, joint_angle: float):
         if joint_type == "Servo":
@@ -82,14 +82,20 @@ class SimVis:
         ):
             self.set_joint(joint_name, "Servo", joint_value)
         self.set_joint("gripper_joint", "Gripper", joints[-1])
-        mujoco.mj_step(self.model, self.data)
-        self.viewer.sync()
+        mujoco.mj_forward(self.model, self.data)
 
     def run_viewer(self):
+        # Initialize the viewer
         self.viewer.cam.distance = 3
+
         self.viewer.sync()
+        self.last_sync_time = time.time()
+        # Main viewer loop (refresh based on target FPS)
         while self.viewer.is_running():
-            self.viewer.sync()
+            curr_time = time.time()
+            if curr_time - self.last_sync_time > self.visualization_refresh_rate:
+                self.viewer.sync()
+                self.last_sync_time = curr_time
 
 
 if __name__ == "__main__":
